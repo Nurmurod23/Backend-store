0ea885a26e9a1acacbee91cec23cdf3ce52f5bc5
Nurmurod23 <elow9020@gmail.com>
2024-08-31 23:28:47 +0500

    fist commit

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const dbURI = 'mongodb+srv://elow9020:zuyX6QNpuBTbfcIP@cluster0.zi74q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
+    const dbURI = 'mongodb+srv://elow9020:zuyX6QNpuBTbfcIP@cluster0.zi74q.mongodb.net/backend-store?retryWrites=true&w=majority&appName=Cluster0'; 
     await mongoose.connect(dbURI);
 
     console.log('MongoDB Connected...');
